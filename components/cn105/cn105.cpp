defed66251e06db8c4347842c10983648daf34a8
ntableman <nathan@tableman.com>
2024-02-15 16:53:16 -0800

    fsd

--- components/cn105/cn105.cpp
+++ components/cn105/cn105.cpp
@@ -38,9 +38,9 @@ CN105Climate::CN105Climate(uart::UARTComponent* uart) :
         climate::CLIMATE_SWING_VERTICAL,
         climate::CLIMATE_SWING_OFF });
 
-    //this->cool_setpoint = 20.0;
-    //this->heat_setpoint = 20.0;
-    //this->auto_setpoint = 20.0;
+    this->cool_setpoint = -1.0;
+    this->heat_setpoint = -1.0;
+    this->auto_setpoint = -1.0;
 
     this->isConnected_ = false;
     this->tempMode = false;
