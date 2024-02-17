e3cb7ccd0e3e5d02811a6a65e05a07e5a6565760
ntableman <nathan@tableman.com>
2024-02-16 17:34:33 -0800

    sdf

--- components/cn105/hp_readings.cpp
+++ components/cn105/hp_readings.cpp
@@ -148,9 +148,9 @@ void CN105Climate::getPowerFromResponsePacket() {
     ESP_LOGD("Decoder", "[0x09 is sub modes]");
 
     heatpumpSettings receivedSettings{};
-    receivedSettings.stage = lookupByteMapValue(STAGE_MAP, STAGE, 2, data[4], "current stage for delivery");
-    receivedSettings.sub_mode = lookupByteMapValue(SUB_MODE_MAP, SUB_MODE, 2, data[3], "submode");
-    receivedSettings.auto_sub_mode = lookupByteMapValue(AUTO_SUB_MODE_MAP, AUTO_SUB_MODE, 2, data[5], "auto mode sub mode");
+    receivedSettings.stage = lookupByteMapValue(STAGE_MAP, STAGE, 5, data[4], "current stage for delivery");
+    receivedSettings.sub_mode = lookupByteMapValue(SUB_MODE_MAP, SUB_MODE, 4, data[3], "submode");
+    receivedSettings.auto_sub_mode = lookupByteMapValue(AUTO_SUB_MODE_MAP, AUTO_SUB_MODE, 4, data[5], "auto mode sub mode");
 
     ESP_LOGD("Decoder", "[Stage : %s]", receivedSettings.stage);
     ESP_LOGD("Decoder", "[Sub Mode  : %s]", receivedSettings.sub_mode);
