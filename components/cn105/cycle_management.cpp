40850d65208cc08a4f1752865394c353af4e50e9
Eric Chavet <echavet@gmail.com>
2024-02-23 17:28:43 +0100

    tunning

--- components/cn105/cycle_management.cpp
+++ components/cn105/cycle_management.cpp
@@ -59,5 +59,5 @@ bool cycleManagement::hasUpdateIntervalPassed(unsigned int update_interval) {
 
 bool cycleManagement::doesCycleTimeOut(unsigned int update_interval) {
     if (CUSTOM_MILLIS < lastCycleStartMs) return false;         // must be checked because operands are they are unsigned
-    return (CUSTOM_MILLIS - lastCycleStartMs) > (3 * update_interval) + 1000;
+    return (CUSTOM_MILLIS - lastCycleStartMs) > (2 * update_interval) + 1000;
 }
