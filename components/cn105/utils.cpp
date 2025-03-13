d3e4a852f68a8088cff97bb97cf9273dbb0ed3e2
Nat Gist <25966140+ngist@users.noreply.github.com>
2025-03-13 15:16:21 +0000

    Update utils.cpp
    add scope resolultion

--- components/cn105/utils.cpp
+++ components/cn105/utils.cpp
@@ -3,14 +3,14 @@
 
 using namespace esphome;
 
-void log_info_uint32(const char* tag, const char* msg, uint32_t value, const char* suffix) {
+void esphome::log_info_uint32(const char* tag, const char* msg, uint32_t value, const char* suffix) {
 #if __GNUC__ >= 11
     ESP_LOGI(tag, "%s %lu %s", msg, (unsigned long)value, suffix);
 #else
     ESP_LOGI(tag, "%s %u %s", msg, (unsigned int)value, suffix);
 #endif
 }
-void log_debug_uint32(const char* tag, const char* msg, uint32_t value, const char* suffix) {
+void esphome::log_debug_uint32(const char* tag, const char* msg, uint32_t value, const char* suffix) {
 #if __GNUC__ >= 11
     ESP_LOGD(tag, "%s %lu %s", msg, (unsigned long)value, suffix);
 #else
