27f7e870d9875d82a96b6c0130c2d504554cb914
Giovanni <105289353+GioMez@users.noreply.github.com>
2026-03-19 12:17:35 +0100

    Remove deprecated C++ setter calls from sensor headers

--- components/cn105/compressor_frequency_sensor.h
+++ components/cn105/compressor_frequency_sensor.h
@@ -9,8 +9,6 @@ namespace esphome {
     class CompressorFrequencySensor : public sensor::Sensor, public Component {
     public:
         CompressorFrequencySensor() {
-            this->set_unit_of_measurement("Hz");
-            this->set_device_class("frequency");
             this->set_state_class(sensor::StateClass::STATE_CLASS_MEASUREMENT);
             this->set_accuracy_decimals(1);
         }
